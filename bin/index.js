359c10f2f9e5781f75d413aaa545709872478665
Yeasir H <yeasir01@gmail.com>
2022-01-28 15:19:34 -0800

    finished query builder

--- bin/index.js
+++ bin/index.js
@@ -4,33 +4,38 @@ import chalkAnimation from "chalk-animation"
 import axios from "axios";
 import inquirer from 'inquirer';
 
+const BASE_URL = 'https://opentdb.com/api.php';
+const TOKEN_URL = 'https://opentdb.com/api_token.php?command=request';
+const CATEGORY_URL = 'https://opentdb.com/api_category.php';
+
+let questions;
+let urlQuery;
+
 const log = console.log;
 const sleep = (ms = 2000) => new Promise(r => setTimeout(r, ms));
-
-let categories;
-let questions;
-let urlQuery = 'https://opentdb.com/api.php?';
 
 function handleError(err) {
     log(chalk.redBright(err?.message || err?.msg))
 }
 
-async function getSessionToken(){
+async function getSessionToken(url) {
     try {
-        const { data } = await axios.get('https://opentdb.com/api_token.php?command=request')
-        urlQuery += `&token=${data.token}`;
+        const { data } = await axios.get(url)
+        return {
+            token: data.token
+        };
     } catch (err) {
-        handleError(err);
+        throw(err);
     }
 }
 
-async function getCategories(){
+async function getCategoryList(url) {
     try {
-        const { data } = await axios.get('https://opentdb.com/api_category.php')
+        const { data } = await axios.get(url);
         const arry = data["trivia_categories"];
-        categories = arry.map(item=>({name: item.name, value: item.id}));
+        return arry.map(item => ({ name: item.name, value: item.id }));
     } catch (err) {
-        handleError(err);
+        throw(err)
     }
 }
 
@@ -38,7 +43,7 @@ async function welcome() {
     try {
         console.clear();
         const title = chalkAnimation.rainbow('WELCOME TO QUIZ-ME!', 4);
-        const msg = `The best command line trivia games are now in your terminal!`;
+        const msg = `The best trivial questions, now in your terminal!`;
         await sleep();
         title.stop();
         log(chalk.italic.gray(msg));
@@ -56,38 +61,69 @@ async function getQuestions(url) {
     }
 };
 
-async function userOptions() {
+async function getUserInput() {
     try {
-        const userInput = await inquirer.prompt([{
+        let categories = await getCategoryList(CATEGORY_URL);
+
+        let userInput = await inquirer.prompt([{
             type: 'number',
             name: 'amount',
             message: 'Number of questions?'
-        },{
+        }, {
             type: 'rawlist',
             name: 'category',
             message: 'Choose a category',
-            choices: [...categories]
-        },{
+            choices: categories
+        }, {
             type: 'list',
             name: 'difficulty',
             message: 'difficulty?',
-            choices: ["easy", "medium", "hard"]
-        },{
+            choices: [{ 
+                name: "Easy", 
+                value: "easy" 
+            }, { 
+                name: "Medium", 
+                value: "medium" 
+            }, { 
+                name: "Hard", 
+                value: "hard" 
+            }]
+        }, {
             type: 'list',
             name: 'type',
             message: 'Question types?',
-            choices: [{name: "True/False", value: 'boolean'}, {name: "Multiple Choice", value: 'multiple'}]
+            choices: [{ 
+                name: "True/False", 
+                value: 'boolean' 
+            }, { 
+                name: "Multiple Choice", 
+                value: 'multiple' 
+            }]
         }])
+        return userInput;
+    } catch (err) {
+        handleError(err);
+    }
+}
+
+function getQueryString(obj){
+    return Object.keys(obj).map(key => key + '=' + obj[key]).join('&');
+}
+
+async function buildURLQuery() {
+    try {
+        const token = await getSessionToken(TOKEN_URL);
+        const userInputObj = await getUserInput();
+        const queryString = getQueryString({...userInputObj, ...token});
+
+        urlQuery = BASE_URL + "?" + queryString;
     } catch (err) {
         handleError(err);
     }
 }
 
 await welcome();
-await getSessionToken();
-await getCategories();
-await userOptions();
+await buildURLQuery();
 //await getQuestions(urlQuery);
 
-log(urlQuery)
-//log(questions)
+log(urlQuery);
